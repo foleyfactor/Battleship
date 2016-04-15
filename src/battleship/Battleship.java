11305e648e056759b3be98cfe57cc68150c69656
Alex Foley <foleyfactor98@gmail.com>
2016-04-15 08:46:05 -0400

    Changed Comments
    Now they aren't auto-generated!

--- src/battleship/Battleship.java
+++ src/battleship/Battleship.java
@@ -1,18 +1,12 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * Written by Islam Elmallah, Alex Foley, James Milne for the 
+ * ICS4UI Software Design Project
  */
 package battleship;
 
-/**
- *
- * @author Alex
- */
 public class Battleship {
 
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
         // TODO code application logic here
     }
