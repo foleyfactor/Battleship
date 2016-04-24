ec08f51d73aad8ea3ce51968dff563c651b94955
jamessss123 <james_milne@rogers.com>
2016-04-24 18:21:40 -0400

    updated start screen, to make it so that if a difficulty was selected, the game would start, but couldn't quite get that to work

--- src/battleship/Battleship.java
+++ src/battleship/Battleship.java
@@ -9,13 +9,21 @@ import javax.swing.JFrame;
 public class Battleship {
     
     public static void main(String[] args) {
-        GameScreen g1 = new GameScreen();
-        g1.getBigBoard().setShipsToBePlaced(new Ship[] {new Ship(2, true), new Ship(3, false), new Ship(4, false), new Ship(5, true)});
-        g1.getSmallBoard().setShipsToBePlaced(new Ship[] {new Ship(2, true), new Ship(3, false), new Ship(4, false), new Ship(5, true)});
-        g1.getBigBoard().setOBoard(g1.getSmallBoard());
-        //g1.getSmallBoard().setAI(new AI(g1.getBigBoard()));
-        g1.getBigBoard().setAI(new AI(g1.getSmallBoard()));
-        g1.getBigBoard().setKeyBindings();
-        g1.setVisible(true);
+        StartScreen s1 = new StartScreen();
+        s1.setVisible(true);
+        
+        if (s1.canStart()==true){
+            GameScreen g1 = new GameScreen();
+            g1.getBigBoard().setShipsToBePlaced(new Ship[] {new Ship(2, true), new Ship(3, false), new Ship(4, false), new Ship(5, true)});
+            g1.getSmallBoard().setShipsToBePlaced(new Ship[] {new Ship(2, true), new Ship(3, false), new Ship(4, false), new Ship(5, true)});
+            g1.getBigBoard().setOBoard(g1.getSmallBoard());
+            //g1.getSmallBoard().setAI(new AI(g1.getBigBoard()));
+            g1.getBigBoard().setAI(new AI(g1.getSmallBoard()));
+            g1.getBigBoard().setKeyBindings();
+            g1.setVisible(true);
+        } else {
+        }
+        
+        
     }
 }
--- src/battleship/StartScreen.java
+++ src/battleship/StartScreen.java
@@ -8,13 +8,25 @@ package battleship;
  *
  * @author milnj5934
  */
-public class StartScreen extends javax.swing.JFrame {
 
-    /**
-     * Creates new form StartScreen
-     */
+public class StartScreen extends javax.swing.JFrame {
+    String difficulty;
+    boolean difficultySelected;
+
+    
     public StartScreen() {
+        
         initComponents();
+        
+    }
+    //Determines if a difficulty has been selected so the player can advance to the game screen
+    public boolean canStart(){
+        if (difficultySelected = true){
+            return true;
+        }
+        else{
+            return false;
+        }
     }
 
     /**
@@ -26,54 +38,128 @@ public class StartScreen extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jButton1 = new javax.swing.JButton();
-        jButton2 = new javax.swing.JButton();
+        jPanel1 = new javax.swing.JPanel();
+        jPanel2 = new javax.swing.JPanel();
         jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        EasyMode = new javax.swing.JButton();
+        MediumMode = new javax.swing.JButton();
+        HardMode = new javax.swing.JButton();
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 100, Short.MAX_VALUE)
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 100, Short.MAX_VALUE)
+        );
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-        setBackground(new java.awt.Color(51, 153, 255));
+        setTitle("Battleship");
+        setBackground(new java.awt.Color(255, 0, 0));
         setForeground(new java.awt.Color(0, 153, 255));
 
-        jButton1.setFont(new java.awt.Font("Tahoma", 1, 24)); // NOI18N
-        jButton1.setText("1 Player");
-
-        jButton2.setFont(new java.awt.Font("Tahoma", 1, 24)); // NOI18N
-        jButton2.setText("2 Players");
+        jPanel2.setBackground(new java.awt.Color(0, 153, 255));
 
         jLabel1.setFont(new java.awt.Font("Impact", 2, 48)); // NOI18N
         jLabel1.setText("Battleship");
+
+        jLabel2.setFont(new java.awt.Font("Impact", 0, 18)); // NOI18N
+        jLabel2.setText("Choose Difficulty");
+
+        EasyMode.setFont(new java.awt.Font("Agency FB", 1, 36)); // NOI18N
+        EasyMode.setText("Easy");
+        EasyMode.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                EasyModeMouseClicked(evt);
+            }
+        });
+
+        MediumMode.setFont(new java.awt.Font("Agency FB", 1, 36)); // NOI18N
+        MediumMode.setText("Medium");
+        MediumMode.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                MediumModeMouseClicked(evt);
+            }
+        });
+
+        HardMode.setFont(new java.awt.Font("Agency FB", 1, 36)); // NOI18N
+        HardMode.setText("Hard");
+        HardMode.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                HardModeMouseClicked(evt);
+            }
+        });
+
+        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
+        jPanel2.setLayout(jPanel2Layout);
+        jPanel2Layout.setHorizontalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel2Layout.createSequentialGroup()
+                        .addGap(273, 273, 273)
+                        .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 221, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(jPanel2Layout.createSequentialGroup()
+                        .addGap(310, 310, 310)
+                        .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 132, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(MediumMode, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(EasyMode, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(HardMode, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))))
+                .addContainerGap(277, Short.MAX_VALUE))
+        );
+        jPanel2Layout.setVerticalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addGap(28, 28, 28)
+                .addComponent(jLabel1)
+                .addGap(18, 18, 18)
+                .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 28, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(20, 20, 20)
+                .addComponent(EasyMode)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(MediumMode)
+                .addGap(18, 18, 18)
+                .addComponent(HardMode)
+                .addContainerGap(88, Short.MAX_VALUE))
+        );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addContainerGap(297, Short.MAX_VALUE)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
-                            .addComponent(jButton2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                            .addComponent(jButton1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                        .addGap(300, 300, 300))
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                        .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 311, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(176, 176, 176))))
+            .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addGap(38, 38, 38)
-                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 76, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(44, 44, 44)
-                .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 68, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(36, 36, 36)
-                .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 64, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(65, Short.MAX_VALUE))
+            .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
+    private void EasyModeMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_EasyModeMouseClicked
+        difficulty = "easy";
+        difficultySelected = true;
+        
+    }//GEN-LAST:event_EasyModeMouseClicked
+
+    private void MediumModeMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_MediumModeMouseClicked
+         difficulty = "medium";
+         difficultySelected = true;
+    }//GEN-LAST:event_MediumModeMouseClicked
+
+    private void HardModeMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_HardModeMouseClicked
+         difficulty = "hard";
+         difficultySelected = true;
+    }//GEN-LAST:event_HardModeMouseClicked
+    
+    
+    
     /**
      * @param args the command line arguments
      */
@@ -104,13 +190,19 @@ public class StartScreen extends javax.swing.JFrame {
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
+                
                 new StartScreen().setVisible(true);
+                
             }
         });
     }
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButton1;
-    private javax.swing.JButton jButton2;
+    private javax.swing.JButton EasyMode;
+    private javax.swing.JButton HardMode;
+    private javax.swing.JButton MediumMode;
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel jPanel2;
     // End of variables declaration//GEN-END:variables
 }
