c0a84bc34b9f80ed339346db0b4db2ca44723019
Alex Foley <foleyfactor98@gmail.com>
2016-04-25 08:43:18 -0400

    Updated ships to be the real battleships

--- src/battleship/Battleship.java
+++ src/battleship/Battleship.java
@@ -8,14 +8,16 @@ import javax.swing.JFrame;
 
 public class Battleship {
     
+    public static final Ship[] battleships = new Ship[] {new Ship(5, true), new Ship(4, true), new Ship(3, true), new Ship(3, true), new Ship(2, true)};
+    
     public static void main(String[] args) {
         StartScreen s1 = new StartScreen();
         s1.setVisible(true);
         
         if (s1.canStart()==true){
             GameScreen g1 = new GameScreen();
-            g1.getBigBoard().setShipsToBePlaced(new Ship[] {new Ship(2, true), new Ship(3, false), new Ship(4, false), new Ship(5, true)});
-            g1.getSmallBoard().setShipsToBePlaced(new Ship[] {new Ship(2, true), new Ship(3, false), new Ship(4, false), new Ship(5, true)});
+            g1.getBigBoard().setShipsToBePlaced(battleships);
+            g1.getSmallBoard().setShipsToBePlaced(battleships);
             g1.getBigBoard().setOBoard(g1.getSmallBoard());
             //g1.getSmallBoard().setAI(new AI(g1.getBigBoard()));
             g1.getBigBoard().setAI(new AI(g1.getSmallBoard()));
